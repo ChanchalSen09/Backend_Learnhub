abc3c695edb179f54cea9d4d03d71ea7b19a0a89
Chanchal Sen <92750541+ChanchalSen09@users.noreply.github.com>
2024-10-31 18:57:39 +0530

    Update User.js LoginBug

--- LearnHub_Back/models/User.js
+++ LearnHub_Back/models/User.js
@@ -29,25 +29,7 @@ const userSchema = new mongoose.Schema({
   }]
 });
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
 
-  try {
-    const hashedPassword = await bcrypt.hash(this.password, 10);
-    this.password = hashedPassword;
-    next();
-  } catch (error) {
-    next(error);
-  }
-});
-
-userSchema.methods.comparePassword = async function (candidatePassword) {
-  try {
-    return await bcrypt.compare(candidatePassword, this.password);
-  } catch (error) {
-    throw new Error("Error comparing passwords");
-  }
-};
 
 const User = mongoose.model("User", userSchema);
 
